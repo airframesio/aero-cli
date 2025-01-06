ca46f736e7d1bdb6c0c62343b4f39c4c1c0ec088
Andy <andy@airframes.io>
2025-01-06 18:59:22 -0500

    Add MSK/OQPSK demodulator

---
+++ decode/fftwrapper.h
@@ -0,0 +1,23 @@
+#ifndef FFTWRAPPER_H
+#define FFTWRAPPER_H
+
+#include "jfft.h"
+#include <QVector>
+#include <complex>
+
+// underlying fft still uses the type in the  kiss_fft_type in the c stuff
+template <typename T> class FFTWrapper {
+public:
+  FFTWrapper(int nfft, bool inverse, bool kissfft_scaling = true);
+  ~FFTWrapper();
+  void transform(const QVector<std::complex<T>> &in,
+                 QVector<std::complex<T>> &out);
+
+private:
+  JFFT fft;
+  int nfft;
+  bool inverse;
+  bool kissfft_scaling;
+};
+
+#endif // FFTWRAPPER_H
